8672744e190234e8b1efa33783024df09e85281e
Ali Mohiuddin <54055822+somethingdevs@users.noreply.github.com>
2024-07-18 16:49:23 -0400

    final project

---
+++ src/components/UI/Input.jsx
@@ -0,0 +1,14 @@
+export default function Input({ label, id, ...props }) {
+  return (
+    <p className="control">
+      <label htmlFor={id}>{label}</label>
+      <input
+        id={id}
+        type="text"
+        name={id}
+        {...props}
+        required
+      />
+    </p>
+  );
+}
